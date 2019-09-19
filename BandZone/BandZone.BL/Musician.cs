e53696448f88c93f82230a011d7a68abb1244bd3
Hunter <700189486@fvtc.edu>
2019-09-19 17:28:26 -0500

    Added Load onto musician

--- BandZone/BandZone.BL/Musician.cs
+++ BandZone/BandZone.BL/Musician.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BandZone.PL;
 
 namespace BandZone.BL
 {
@@ -15,7 +16,43 @@ namespace BandZone.BL
         public string ContactEmail { get; set; }
         public string Website { get; set; }
         public string ProfileImage { get; set; }
+        public string LoginEmail { get; set; }
+        public string Password { get; set; }
 
 
+    }
+
+    public class MusicianList : List<Musician>
+    {
+        public void Load()
+        {
+            try
+            {
+                using (BandZoneEntities dc = new BandZoneEntities())
+                {
+                    var results = dc.tblMusician;
+                    foreach (tblMusician c in results)
+                    {
+                        Musician musician = new Musician
+                        {
+                            MusicianId = c.MusicianId,
+                            BandMusicianName = c.BandMusicianName,
+                            SongId = c.SongId != null ? c.SongId : Guid.Empty,
+                            Phone = c.Phone,
+                            ContactEmail = c.ContactEmail,
+                            Website = c.Website,
+                            LoginEmail = c.LoginEmail,
+                            Password = c.Password
+                        };
+
+                        this.Add(musician);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
     }
 }
