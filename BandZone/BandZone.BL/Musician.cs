d2ad8a18b6156b63cb86e04c0eff30790e0d1cc1
Hunter <700189486@fvtc.edu>
2019-09-19 17:16:04 -0500

    Added BL Classes

---
+++ BandZone/BandZone.BL/Musician.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BandZone.BL
+{
+    public class Musician
+    {
+        public Guid MusicianId { get; set; }
+        public string BandMusicianName { get; set; }
+        public Guid SongId { get; set; }
+        public string Phone { get; set; }
+        public string ContactEmail { get; set; }
+        public string Website { get; set; }
+        public string ProfileImage { get; set; }
+
+
+    }
+}
