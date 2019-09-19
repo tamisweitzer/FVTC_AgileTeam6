793d8d6f4554d5cef006b08ed3153d16f6492465
Tami Sweitzer <tamara0715@gmail.com>
2019-09-19 16:45:23 -0500

    Start adding Controllers for view

--- BandZone/BandZone.UI/Controllers/HomeController.cs
+++ BandZone/BandZone.UI/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace BandZone.UI.Controllers
 {
     public class HomeController : Controller
---
+++ BandZone/BandZone.UI/Controllers/MusicianController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+// add using statement to BL, ViewModels, & Models
+
+namespace BandZone.UI.Controllers
+{
+    public class MusicianController : Controller
+    {
+        // GET: Musician
+        public ActionResult Index()
+        {
+            if (Authenticate.IsAuthenticated())
+            {
+                musician = new Musician();
+                musician.load()
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Create", "Login", new { returnurl = HttpContext.Request.Url });
+            }
+            
+        }
+    }
+}
