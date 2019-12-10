35db689e2b2ee60bfaf51680a5e2c497f4847a6d
Tami Sweitzer <tamara0715@gmail.com>
2019-12-10 14:03:58 -0600

    Refreshed local copy of tables from DB

--- BandZone/BandZone.PL/Bandzone.Designer.cs
+++ BandZone/BandZone.PL/Bandzone.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Eli\Documents\BandZ\BandZone\BandZone.PL\Bandzone.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\repos\Agile\BandZone\BandZone.PL\Bandzone.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
